6b6eaddf44098d398328982c82ff8f0607414024
GGavrovski <goran.gavrovski@gmail.com>
2022-04-13 22:15:32 +0200

    update deps

--- next.config.js
+++ next.config.js
@@ -3,17 +3,18 @@
 const path = require("path");
 
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  images: {
-    domains: ["assets"],
-    formats: ["image/avif", "image/webp"],
-  },
-  compilerOptions: {
-    baseURL: "src",
-  },
+	reactStrictMode: true,
+	optimizeFonts: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
+	images: {
+		domains: ["assets"],
+		formats: ["image/png", "image/svg"],
+	},
+	compilerOptions: {
+		baseURL: "src",
+	},
 };
 
 module.exports = nextConfig;
